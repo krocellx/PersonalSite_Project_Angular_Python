3e7f66daf2c6762430641dca7d5ff2717dc52c22
krocellx <harvey_xie@hotmail.com>
2022-04-07 00:42:58 -0400

    backend db setup

---
+++ backend/bootstrap.bat
@@ -0,0 +1,3 @@
+set FLASK_APP=./src/main.py
+call "C:/Users/Harvey/.virtualenvs/backend-Pz2R_v-6/Scripts/activate.bat“
+flask run -h 0.0.0.0
---
+++ backend/bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+$env:FLASK_APP=./src/main.py
+#source $(pipenv --venv)/Scripts
+C:/Users/Harvey/.virtualenvs/backend-Pz2R_v-6/Scripts/activate.bat
+flask run -h 0.0.0.0
