d429ed4c4443d5a0fabd5d4b438c2737384d50b4
Krocell <harvey_xie@hotmail.com>
2022-04-14 16:16:48 -0400

    update script

--- backend/bootstrap.sh
+++ backend/bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-$env:FLASK_APP=./src/main.py
-#source $(pipenv --venv)/Scripts
-C:/Users/Harvey/.virtualenvs/backend-Pz2R_v-6/Scripts/activate.bat
-flask run -h 0.0.0.0
+export FLASK_APP=./src/main.py
+source $(pipenv --venv)/bin/activate
+flask run #-h 0.0.0.0nvm install node
